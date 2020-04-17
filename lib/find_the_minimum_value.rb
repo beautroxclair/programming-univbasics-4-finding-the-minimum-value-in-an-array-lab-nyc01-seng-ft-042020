4b383d1e7f3a2101a9da221554ae2d4fad668b34
Beau Troxclair <beau.troxclair@gmail.com>
2020-04-17 03:01:29 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,10 +1,8 @@
 def find_min_value(array)
   count = 0
   min = array[count]
-  while count < array.length do
+  while count < array.length - 1 do
     count += 1
-    puts array[count]
-    puts min
     if array[count] < min
       min = array[count]
     end
